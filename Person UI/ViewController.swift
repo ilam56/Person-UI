f5ccde55d514578b5a0090301f566939b0b10747
Ian Anderson <ilam56@mail.missouri.edu>
2019-09-12 21:39:51 -0500

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Ian Anderson on 9/12/19.
+//  Copyright © 2019 Ian Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
